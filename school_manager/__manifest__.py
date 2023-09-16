49969040d1a1e5e2b59de5241154bf24d8560ec4
luctp <luctrph@gmail.com>
2023-09-16 16:00:27 +0700

    update student manager

--- school_manager/__manifest__.py
+++ school_manager/__manifest__.py
@@ -13,7 +13,6 @@
     'data': ['views/school_view.xml',
              'views/student_view.xml',
              'views/subject_view.xml',
-             'views/subject.xml',
              ],
     'demo': [],
     'installable': True,
